ea4f4929329cb9531b50acd21e7274e45ce4b0d3
mettli <mettli@mettlis-MacBook-Pro.local>
2023-10-10 09:03:18 +0800

    102.binary tree level order traversal

---
+++ javascript/0102.binary-tree-level-order-traversal.js
@@ -0,0 +1,71 @@
+// Created by mettli at 2023/10/10 08:39
+// leetgo: 1.3.8
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+/*
+102. Binary Tree Level Order Traversal (Medium)
+Given the `root` of a binary tree, return the level order traversal of its nodes' values. (i.e.,
+from left to right, level by level).
+
+**Example 1:**
+
+![](https://assets.leetcode.com/uploads/2021/02/19/tree1.jpg)
+
+```
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+```
+
+**Example 2:**
+
+```
+Input: root = [1]
+Output: [[1]]
+```
+
+**Example 3:**
+
+```
+Input: root = []
+Output: []
+```
+
+**Constraints:**
+
+- The number of nodes in the tree is in the range `[0, 2000]`.
+- `-1000 <= Node.val <= 1000`
+
+*/
+
+// @lc code=begin
+
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if(!root) {
+        return [];
+    }
+
+    const nodes = [root];
+    const values = [];
+
+    while(nodes.length) {
+        const len = nodes.length;
+        const currentLevelValue = [];
+
+        for(let i = 0; i < len; i++) {
+            const node = nodes.shift();
+            currentLevelValue.push(node.val);
+            if(node.left) nodes.push(node.left);
+            if(node.right) nodes.push(node.right);
+        }
+
+        values.push(currentLevelValue);
+    }
+
+    return values;
+};
+
+// @lc code=end
